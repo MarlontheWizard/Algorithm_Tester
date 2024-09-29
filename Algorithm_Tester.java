583fc47e1992bc524b5b9896790a7a2823ca0304
MarlontheWizard <marlon.dominguez307@gmail.com>
2024-09-29 04:46:20 -0400

    SMALL UPDATE: Added additional comments, implemented terminal display of all constructors associated with the object being used for test cases,organized code. Next large update shall implement user selection of constructor and handle input data of arguments. ~MarlontheWizard

--- Algorithm_Tester.java
+++ Algorithm_Tester.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 public interface Algorithm_Tester <Type>{
     
     /* Generates {numeric} values for test cases. 
@@ -8,33 +12,135 @@ public interface Algorithm_Tester <Type>{
      * int lower_bound -> Specifies lower bound if a range must be satisfied. 
      * int upper_bound -> Specifies upper bound if a range must be satisfied. 
      */ 
-    public void numeric_generator(boolean signed, int cardinality, int lower_bound, int upper_bound){
+    public void primitive_obj_generator(boolean signed, int cardinality, int lower_bound, int upper_bound){
 
 
-
-
+        
     }
     
 
-    public int[] obj_generator(<Type> obj_type, int cardinality, int lower_bound, int upper_bound){
+    public int[] nonprimitive_obj_generator(<Type> obj_type, int cardinality, int lower_bound, int upper_bound){
 
-        String obj = obj_type.getClass().getSimpleName();
+
+        String obj_className = obj_type.getClass().getName();
+
+        //Display non-primitive data type being used to generate test cases 
+        System.out.println("Generating test values of type {" + obj + "}"); 
 
 
         /*
          * Generally, the forName() class method imports the target 
          * object's identity/contents so that the JVM recognizes it to be initialized here. 
          */
-        Class<?> obj_class = Class.forName(obj); 
+
+        System.out.println("Importing object...");
+
+        Class<?> obj_class = null;
+
+        try{
+
+            obj_class = Class.forName(obj_className); 
+        }
+
+        catch(ClassNotFoundException class_error){
+
+            System.out.println("Could not find the object's class. Terminating program...");
+            System.exit(1);
+        }
+
+        System.out.println("Object imported. Displaying all available constructors...");
+
 
         /*
-         *The JVM now recognizes the class, or the . we can dynamically allocate it.
+         *The JVM now recognizes the class, or the object definition. We can now dynamically allocate it.
          *
-         *A class can have many constructors with varying paramaters. 
-         *
-          
-        Object initialized_obj = obj_class.getDeclaredConstructor()
+         *An nonprimitive object can have many constructors with varying parameters. 
+         *Therefore, our algorithm tester must present all of the possible 
+         *constructors and their associated parameters so that the user can provide the 
+         *correct information needed to begin creating test cases with the object. 
+         */
+
+          dynamic_allocation_byUser(); 
+        
+
+        //Create logic to accept input and handle as needed. Remember, we want to dynamically allocate the object.
+        
 
 
+        //We now know the object type, preferred constructor and parameters to be entered. Begin generating. 
+
+        Object initialized_obj = obj_class.getDeclaredConstructor();
+
+
+    }
+
+    public Parameter<?>[] dynamic_allocation_byUser(){
+        
+        
+
+    }
+
+
+
+    public void display_class_constructors(Class<?> obj_class){
+
+        /*
+        It is possible that the user already knows the specific constructor they want to use, 
+        in other words, the arguments. 
+        */
+        
+
+        /*User does not know constructor in advance -> Let's display constructors and their parameters.
+          To access the constructors of a class we must use the Method subclass of Reflection. 
+          Attributes from the Method class will allow us to retrieve all methods associated with our 
+          non-primitive object, but we only want the constructors. 
+        */
+        display_class_constructors(constructors);
+
+
+        //Get all possible constructors & associated parameters, then display them if necessary. 
+        Constructor<?>[] obj_constructors = null;
+        
+        try{
+            
+            constructors = obj_class.getConstructors(); 
+        }
+
+        catch(SecurityException sException){
+
+            System.out.println("Could not retrieve constructors from object class. Terminating tester...");
+            System.exit(1);
+        }
+
+
+        System.out.println("Method Signature\t\tParameters\t\t\tTypes\n----------------\t\t----------\t\t\t-----");
+        for(Constructor<?> constructor : obj_constructors){
+
+            System.out.printf("%-32s", method.getName());
+            
+            Parameter[] params = constructor.getParameters();
+            Class<?>[]  param_types = constructor.getParameterTypes();
+
+            String param_str = "";
+
+            for(Parameter param : params){
+
+                param_str += param.getName() + " "; 
+            }
+
+            System.out.printf("%-28s", param_str);
+            
+            for(Class<?> param_type : param_types){
+
+                System.out.printf("%-5s", param_type.getSimpleName());
+               
+            }
+            
+
+            System.out.println();
+
+        }
+        
+        return;
     }
 }
